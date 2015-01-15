d1d98dd45710011a04f6bf44cae753beade6eddc
Karan Sood <karansood87@gmail.com>
2015-01-16 01:40:52 +0530

    Adding sample data set and more enhancements

---
+++ dataset.js
@@ -0,0 +1,37 @@
+function getEvents(){
+
+	return [
+		{
+			id : 'E01',
+			title : 'Meeting with BA',
+			start : '27-10-2014 10:30:00',
+			end : '27-10-2014 11:00:00',
+			backgroundColor: '#443322',
+			textColor : '#FFF'
+		},
+		{
+			id : 'E01',
+			title : 'Lunch',
+			start : '27-10-2014 12:30:00',
+			end : '27-10-2014 13:00:00',
+			backgroundColor: '#12CA6B',
+			textColor : '#FFF'
+		},
+		{
+			id : 'E02',
+			title : 'Customer Appointment',
+			start : '29-10-2014 09:00:00',
+			end : '29-10-2014 09:30:00',
+			backgroundColor: '#34BB22',
+			textColor : '#FFF'
+		},
+		{
+			id : 'E03',
+			title : 'Buddy Time',
+			start : '30-10-2014 11:00:00',
+			end : '30-10-2014 12:30:00',
+			backgroundColor: '#AA3322',
+			textColor : '#FFF'
+		}
+	];
+}
--- easycal.js
+++ easycal.js
@@ -1,7 +1,5 @@
 (function($, window, document, _, undefined){
 
-
-
 	if( typeof Object.create !== 'function'){
 		Object.create = function(obj){
 			function F(){};
@@ -18,76 +16,124 @@
 
 			self.options = options;
 
+			self.momStartDate = moment(this.options.startDate, 'DD-MM-YYYY');
+			self.momMinTime = moment(this.options.minTime, 'HH:mm:ss');
+			self.momMaxTime = moment(this.options.maxTime, 'HH:mm:ss');
+
 			self.launch();
 			
-			self.$elem.find('td').on('click', function(){
+			self.$elem.find('.ec-slot').on('click', function(){
 				self.options.dayClick.apply(self);
 			});
 
 		},
 
 		launch : function(){
-			
 			this.display();
+			this.showEvents();
 		},
 
 		refresh : function(){
 
 		},
 
-		generateHTML : function(){
-
-			var date = moment(this.options.startDate, 'DD-MM-YYYY');
+		mapEventsByDate : function(){
+			var res = {};
+			var events = this.options.events;
+			var date = this.momStartDate.clone().isoWeekday(1);
 			
-			var minTime = moment(this.options.minTime, 'HH:mm:ss');
-			var maxTime = moment(this.options.maxTime, 'HH:mm:ss');
-
-			date.isoWeekday(1);
-
-			var $container = $('<table border="0" cellpadding="0"></table>').addClass('easycal');
-			var $content
-			var cell = '<th></th>';
-			var $header = $('<thead><tr></tr></thead>');
-			for(var i = 0 ; i < 8 ; i++){
-				var $cell = $(cell);
-				var cellContent = '';
-				if(i !== 0){
-					cellContent = date.format(this.options.columnDateFormat);
-					$cell.addClass('ec-day-header');
-					date.add(1, 'd');
-				}else{
-					$cell.css({width: 35});
-				}
-				$cell.html(cellContent);
-				$header.find('tr').append($cell);
-			}
-			$container.append($header);
-
-			var time = minTime.clone();	
-
-			for(;time.isBefore(maxTime);){
-				var $tr = $('<tr class="tr-body"></tr>');
-				for(var col = 0 ; col < 8 ; col++){
-					var $td = $('<td class="td-body"></td>');
-					var cellContent = '';
-					if(col === 0){
-						cellContent = time.format(this.options.timeFormat);
-						time.add(this.options.slotDuration,'m');
-						$td.addClass('ec-time');
-					}else{
-						$td.addClass('ec-content');
+			for(var i = 0 ; i < 7 ; i++){
+				var dateStr = date.format('DD-MM-YYYY');
+				var filteredEvents = _.filter(events, function(event){
+					if((moment(event.start, 'DD-MM-YYYY HH:mm:ss').format('DD-MM-YYYY')) === dateStr){
+						return true;
 					}
-					$td.html(cellContent);
-					$tr.append($td);
-				}
-				$container.append($tr);
+				});
+				res[dateStr] = filteredEvents;
+				date.add(1, 'd');
 			}
+			return res;
+		},
 
-			return $container;
+		showEvents : function(){
+			var self = this;
+			var events = this.options.events;
+			var $cols = this.$elem.find('table td.ec-slot-col');
+
+			var eventDateMap = this.mapEventsByDate();
+
+			var $col = null, $slots, schedule, slotTime;
+			_.each($cols, function(col, i){
+				$col = $(col); 
+				var colDate = $col.attr('data-date');
+				var dayEvents = eventDateMap[colDate];
+				
+				if(dayEvents.length){
+					schedule = self.getDaySchedule(dayEvents);
+					console.log('Schedule for date : ' + colDate);
+					console.log(schedule);
+
+					$slots = $col.find('.ec-slot');
+					_.each($slots, function(slot, i){
+						slotTime = $(slot).attr('data-time');
+						var scheduleForSlot = schedule[slotTime];
+						if(scheduleForSlot.length > 1){
+							$(slot).css({
+								'background-color' : '#ABC',
+								color : '#000'
+							})
+								.html('<div>Multiple</div>');
+						}else if(scheduleForSlot.length){
+							$(slot).css({
+								'background-color' : scheduleForSlot[0].backgroundColor,
+								color : scheduleForSlot[0].textColor
+							});
+							var slotStartTime = moment(colDate + ' ' + slotTime, 'DD-MM-YYYY HH:mm:ss');
+							var eventStartTime = moment(scheduleForSlot[0].start, 'DD-MM-YYYY HH:mm:ss');
+							if(slotStartTime.isSame(eventStartTime)){
+								$(slot).html('<div>' + scheduleForSlot[0].title + '</div>');
+							}else{
+								$(slot).css({
+									'border-top' : '1px solid ' + scheduleForSlot[0].backgroundColor
+								});
+							}
+						}
+					});
+				}
+			});
+		},
+
+		getDaySchedule : function(dayEvents){
+			var date = moment(dayEvents[0].start, 'DD-MM-YYYY HH:mm:ss').format('DD-MM-YYYY');
+			var minTime = moment(date + ' ' + this.options.minTime, 'DD-MM-YYYY HH:mm:ss');
+			var maxTime = moment(date + ' ' + this.options.maxTime, 'DD-MM-YYYY HH:mm:ss');
+			var time = minTime.clone();
+
+			var schedule = {};
+
+			var begining = null, end = null;
+			for(;time.isBefore(maxTime);){
+				begining = time.clone();
+				end = begining.clone().add(15, 'm');
+
+				var slotEvents = _.filter(dayEvents, function(event){
+					
+					var eventStart = moment(event.start, 'DD-MM-YYYY HH:mm:ss');
+					var eventEnd = moment(event.end, 'DD-MM-YYYY HH:mm:ss');
+
+					if(eventStart.isBefore(end) && eventEnd.isAfter(begining)){
+						return true;
+					}
+				});
+
+				schedule[time.format('HH:mm:ss')] = slotEvents;
+				time.add(15,'m'); //this.options.slotDuration
+			}
+			return schedule;
 		},
 
 		renderHTML : function(){
-			return '<table border="0" cellpadding="0" cellpadding="0" class="easycal">' +
+			return '<table border="0" cellpadding="0" cellspacing="0" class="easycal">' +
 						'<thead>' +
 							'<tr>' +
 								'<td>' +
@@ -109,43 +155,48 @@
 			var date = moment(this.options.startDate, 'DD-MM-YYYY');
 			date.isoWeekday(1);
 
-			var html = '<table border="0" cellspacing="0" cellpadding="0"><tbody><tr>';
+			var html = '<table border="0" cellspacing="0" cellpadding="0" class="ec-head-table"><tbody><tr>';
 			for(var i = 0 ; i < 8 ; i++){
 				var cellContent = '';
 				if(i !== 0){
 					cellContent = date.format(this.options.columnDateFormat);
-					html += '<th class="ec-day-header">' + cellContent + '</th>';
+					html += '<td class="ec-day-header">' + cellContent + '</td>';
 					date.add(1, 'd');
 				}else{
-					html += '<th style="width:35px;"></th>';
+					html += '<td></td>';
 				}
 			}
 			return html + '</tr></tbody></table>';
 		},
 
 		renderTimeGridHTML : function(){
-			var minTime = moment(this.options.minTime, 'HH:mm:ss');
-			var maxTime = moment(this.options.maxTime, 'HH:mm:ss');
+			var minTime = this.momMinTime;
+			var maxTime = this.momMaxTime;
 			var time = minTime.clone();
 
+			var date = moment(this.options.startDate, 'DD-MM-YYYY');
+			date.isoWeekday(1);
 			
-			var html = '<table border="0" cellspacing="0" cellpadding="0"><tbody><tr>';
+			var html = '<table border="0" cellspacing="0" cellpadding="0" class="ec-time-grid-table"><tbody><tr>';
+
+			var cellContent = null, timeTag = null, colDate = null;
 
 			for(var i = 0 ; i < 8 ; i++){
 				if(i===0){
-					html += '<td style="width:35px;">';
-				}else{
 					html += '<td>';
+				}else{
+					colDate = date.format('DD-MM-YYYY');
+					html += '<td class="ec-slot-col" data-date="' + colDate + '">';
+					date.add(1, 'd');
 				}
-				
 
 				for(;time.isBefore(maxTime);){
-
 					if(i === 0){
-						var cellContent = time.format(this.options.timeFormat);
+						cellContent = time.format(this.options.timeFormat);
 						html += '<div class="table-cell ' + this.options.widgetTimeClass + '">' + cellContent + '</div>';
 					}else{
-						html += '<div class="table-cell ' + this.options.widgetSlotClass + '"></div>';
+						timeTag = time.format('HH:mm:ss');
+						html += '<div class="table-cell ' + this.options.widgetSlotClass + '" data-time="' + timeTag + '"></div>';
 					}
 					time.add(this.options.slotDuration,'m');
 				}
@@ -158,9 +209,7 @@
 		},
 
 		display : function(){
-			var html = this.generateHTML();
-			//this.$elem.html(html);
-
+			var html = this.renderHTML();
 			this.$elem.html(this.renderHTML());
 		}
 	};
@@ -186,6 +235,7 @@
 		slotDuration : 15, //in mins
 		dayClick : null,
 		eventClick : null,
+		events : [],
 
 		widgetHeaderClass : 'ec-day-header',
 		widgetSlotClass : 'ec-slot',
